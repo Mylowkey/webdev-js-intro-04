2ec1fbb8cf0519460cca260c146c27c420c2c4db
Mylowkey <eemq20@gmail.com>
2024-12-29 15:31:36 -0500

    I played with IF/else and went to the inspect feature to verify that it was running properly. Also added the code to verify the age but without any useful function with the button yet, I guess because we let the age value NULL.

--- js/assignment.js
+++ js/assignment.js
@@ -5,13 +5,22 @@ const ageInputEl = document.getElementById("age-input")
 const submissionBtn = document.getElementById("submission-btn");
 
 // create a mutable variable called age and do not assign it a value.
+let age = "";
 
 
 function checkAgeAndRespond() {
     age = parseInt(ageInputEl.value);
     
     // Write your code below
-
+if (age >= 21) {
+    console.log ("You can vote and purchase alchol.");
+}
+else if (age < 21 && age > 18) {
+    console.log("You can vote, but you cannot purchase alcohol.");
+}
+else {
+    console.log ("You cannot vote and you cannot purchase alcohol.");
+}
 }
 
 
--- js/index.js
+++ js/index.js
@@ -1,3 +1,31 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+
+
+
+// let altitude = 2000;
+
+// if (altitude >= 2000) {
+//     console.log ("Safe altitude");
+// }
+// else if (altitude < 2000 && altitude >= 1500) {
+//     console.log("Check altitude, don't go lower");
+// }
+// else {
+//     console.log("Warning, low altitude")
+// }
+
+
+
+// let cleanFilter = false;
+
+// if (cleanFilter === true) {
+//     console.log("Clean water");
+// }
+// else {
+//     console.log("Replace filter");
+// }
+
+
+
